d13fcfa6b61a3f4f149cab7613d2ba76c3492e7d
Rafael <rafael.felix.moraes@gmail.com>
2025-05-15 04:45:55 -0300

    Refactor add function auth:api

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+//use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Routing\Controller;
 
 class AuthController extends Controller
 {
-    //
+    public  function __construct() {
+        $this->middleware('auth:api', [
+        'except' => ['login', 'create', 'unauthorized']
+    ]);
+    }
 }
