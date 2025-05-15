1bc81681af3846c54da7f981229590ef38c48634
Rafael <rafael.felix.moraes@gmail.com>
2025-05-15 03:51:37 -0300

    Refactor add route create user

--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,5 @@ Route::get('/ping', function() {
 Route::post('/auth/login', 'AuthController@login');
 Route::post('/auth/logout', 'AuthController@logout');
 Route::post('/auth/refresh', 'AuthController@refresh');
+
+Route::post('/user', 'AuthController@create');
