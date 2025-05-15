41ee01c3e23ba460afb3b8854a4ad7cde339d665
Rafael <rafael.felix.moraes@gmail.com>
2025-05-15 03:54:15 -0300

    Refactor add route user -> update -> cover

--- routes/api.php
+++ routes/api.php
@@ -14,3 +14,4 @@ Route::post('/auth/refresh', 'AuthController@refresh');
 Route::post('/user', 'AuthController@create');
 Route::post('/user', 'AuthController@update');
 Route::post('/user/avatar', 'AuthController@updateAvatar');
+Route::post('/user/cover', 'UserController@updateCover');
