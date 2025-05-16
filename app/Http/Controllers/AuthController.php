4df55f36699b4f5648e84537ef744316bd38559d
Rafael <rafael.felix.moraes@gmail.com>
2025-05-16 16:50:00 -0300

    Refactor add create user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers;
 
-//use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Routing\Controller;
+use App\Models\User;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
@@ -14,4 +15,52 @@ class AuthController extends Controller
         'except' => ['login', 'create', 'unauthorized']
     ]);
     }
+
+    public function create(Request $request) {
+        // POST *api/user(nome, email, senha e dataNascimento)
+        $array = ['error'=>''];
+
+        $name = $request->input('name');
+        $email = $request->input('email');
+        $password = $request->input('password');
+        $birthdate = $request->input('birthdate');
+        if($name && $email && $password && $birthdate) {
+            if(strtotime($birthdate) === false) {
+                $array['error'] = 'Data de nascimento inválida!';
+                return $array;
+            }
+
+            $emailExists = User::where('email', $email)->count();
+            if($emailExists === 0) {
+                $hash = password_hash($password, PASSWORD_DEFAULT);
+
+                $newUser = new User();
+                $newUser->name = $name;
+                $newUser->email = $email;
+                $newUser->password = $hash;
+                $newUser->birthdate = $birthdate;
+                $newUser->save();
+
+                $token = Auth::attempt([
+                    'email' => $email,
+                    'password' => $password
+                ]);
+
+                if(!$token) {
+                    $array['error'] = 'Ocorreu um erro!';
+                    return $array;
+                }
+
+                $array['token'] = $token;
+            } else {
+                $array['error'] = 'E-mail já cadastrado!';
+            }
+
+        } else {
+            $array['error'] = 'Não enviou todos os dados';
+        }
+
+        return $array;
+    }
+
 }
