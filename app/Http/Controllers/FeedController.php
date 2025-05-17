7b43f9efdfe6c37e6a29039f00deb573eb4e2f44
Rafael <rafael.felix.moraes@gmail.com>
2025-05-17 05:26:31 -0300

    Refactor alterado para laravel 12x

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -13,6 +13,6 @@ class FeedController extends Controller
     public  function __construct() {
         $this->middleware('auth:api');
 
-        $this->loggedUser = auth()->user();
+        $this->loggedUser = Auth::user();
     }
 }
