3e1d68b6d9e30cc1d8e5afae5c89bd99c1554976
Rafael <rafael.felix.moraes@gmail.com>
2025-05-20 19:26:37 -0300

    Refactor add function create post

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Post;
+use Intervention\Image\ImageManager;
 
 class FeedController extends Controller
 {
@@ -14,5 +16,66 @@ class FeedController extends Controller
         $this->middleware('auth:api');
 
         $this->loggedUser = Auth::user();
+    }
+
+    public function create(Request $request) {
+        $array = ['error' => ''];
+        $allowedTypes = ['image/jpg', 'image/jpeg', 'image/png'];        
+
+        $type = $request->input('type');
+        $body = $request->input('body');
+        $photo = $request->file('photo');
+
+        if($type) {
+            switch($type) {
+                case 'text':
+                    if(!$body) {
+                        $array['error'] = 'Texto não enviado!';
+                        return $array;
+                    }
+                break;
+                case 'photo':
+                    if($photo) {
+                        if(in_array($photo->getClientMimeType(), $allowedTypes)) {
+                            $filename = md5(time().rand(0, 9999)).'jpg';
+
+                $destinyPath = public_path('/media/uploads');
+
+                if (!file_exists($destinyPath)) {
+                    mkdir($destinyPath, 0755, true);
+                }
+
+                $manager = ImageManager::gd();
+
+                $img = $manager->read($photo->path())
+                    ->resize(800, null)
+                    ->save($destinyPath . '/' . $filename);
+
+                    $body = $filename;
+                         }
+                    } else {
+                        $array['error'] = 'Arquivo não enviado';
+                        return $array;
+                    }
+                break;
+                default:
+                    $array['error'] = 'Tipo de postagem inexistente!';
+                    return $array;
+                break;
+            }
+
+            if($body) {
+                $newPost = new Post();
+                $newPost->id_user = $this->loggedUser['id'];
+                $newPost->type = $type;
+                $newPost->created_at = date('Y-m-d H:i:s');
+                $newPost->body = $body;
+                $newPost->save();
+            }
+        } else {
+            $array['error'] = 'Dados não enviados!';            
+        }
+        
+        return $array;
     }
 }
