06eb10962cb3b20cf91fb41a992d6ff1ee060bc6
Rafael <rafael.felix.moraes@gmail.com>
2025-05-23 07:22:57 -0300

    Refactor add feed read

--- routes/api.php
+++ routes/api.php
@@ -21,9 +21,10 @@ Route::post('/user', [AuthController::class, 'create']);
 Route::put('/user', [UserController::class, 'update']);
 Route::post('/user/avatar', [UserController::class, 'updateAvatar']);
 Route::post('/user/cover', [UserController::class, 'updateCover']);
-/*
 
-Route::get('/feed', 'FeedController@read');
+
+Route::get('/feed', [FeedController::class, 'read']);
+/*
 Route::get('/user/feed', 'FeedController@userFeed');
 Route::get('/user/{id}/feed', 'FeedController@userFeed');
 
