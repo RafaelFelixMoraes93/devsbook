b82defa78fd6e74f84db8574808423af6849d5c0
Rafael <rafael.felix.moraes@gmail.com>
2025-05-27 03:20:07 -0300

    Refactor add function like and unlike

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use App\Models\PostLike;
 use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -14,5 +16,38 @@ class PostController extends Controller
         $this->middleware('auth:api');
 
         $this->loggedUser = Auth::user();
+    }
+
+    public function like($id) {
+        $array = ['error' => ''];
+
+        $postExists = Post::find($id);
+        if($postExists) {
+            $isLiked = PostLike::where('id_post', $id)
+            ->where('id_user', $this->loggedUser['id'])
+            ->count();
+                if($isLiked > 0) {
+                    $postLike = PostLike::where('id_post', $id)
+                    ->where('is_user', $this->loggedUser['id'])
+                    ->first();
+                    $postLike->delete();
+
+                    $isLiked = false;
+                } else {
+                    $newPostLike = new PostLike();
+                    $newPostLike->id_post = $id;
+                    $newPostLike->id_user = $this->loggedUser['id'];
+                    $newPostLike->created_at = date('Y-m-d H-i-s');
+                    $newPostLike->save();
+
+                    $isLiked = true;
+                }
+
+        } else {
+            $array['error'] = 'Post não existe!';
+            return $array;
+        }
+
+        return $array;
     }
 }
